03f61516dc5dd71ab2b07b76f75c2414e1bf635b
blacklytning <blacklytning@yandex.com>
2025-06-29 13:33:27 +0530

    Add routing support

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { ToastContainer } from 'react-toastify';
+import { Routes, Route } from "react-router";
+import { ToastContainer } from "react-toastify";
 import StudentEnquiryForm from "./components/StudentEnquiryForm";
 import StudentAdmissionForm from "./components/StudentAdmissionForm";
 import FeeReceipt from "./components/FeeReceipt";
@@ -10,19 +11,21 @@ import CoursesFeesManagement from "./components/CoursesFeesManagement";
 import StudentFollowupTracker from "./components/StudentFollowupTracker";
 
 function App() {
-  return (
-    <div>
-      <ToastContainer />
-      <StudentEnquiryForm />
-      <StudentAdmissionForm />
-      <FeeReceipt />
-      <StudentEnquiriesList />
-      <StudentAdmissionsList />
-      <InstituteLogin />
-      <CoursesFeesManagement />
-      <StudentFollowupTracker />
-    </div>
-  );
+    return (
+        <div>
+            <ToastContainer />
+            <Routes>
+                <Route path="/" element={<InstituteLogin />} />
+                <Route path="/enquiry" element={<StudentEnquiryForm />} />
+                <Route path="/admission" element={<StudentAdmissionForm />} />
+                <Route path="/fee-receipt" element={<FeeReceipt />} />
+                <Route path="/enquiry-list" element={<StudentEnquiriesList />} />
+                <Route path="/admission-list" element={<StudentAdmissionsList />} />
+                <Route path="/courses-fees" element={<CoursesFeesManagement />} />
+                <Route path="/followup-tracker" element={<StudentFollowupTracker />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
