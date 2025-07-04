f9069f04a44e4ba0399b726c50d75c43a2f10f7f
blacklytning <blacklytning@yandex.com>
2025-07-04 22:52:31 +0530

    Modularize form options

--- src/components/StudentAdmissionForm.jsx
+++ src/components/StudentAdmissionForm.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useForm, Controller } from "react-hook-form";
-import { districts } from './districts';
+import { CATEGORY, COURSES, DISTRICTS, EDUCATIONAL_QUALIFICATION, LANGUAGES, TIMINGS } from './FormComponents';
 import { FormSelect, AadharInput, FormInput } from './FormComponents';
 import { toast } from 'react-toastify';
 
@@ -269,20 +269,7 @@ const StudentAdmissionForm = () => {
                                     required
                                     register={register}
                                     errors={errors}
-                                    options={[
-                                        { value: "MARATHI", label: "MARATHI" },
-                                        { value: "HINDI", label: "HINDI" },
-                                        { value: "ENGLISH", label: "ENGLISH" },
-                                        { value: "GUJARATI", label: "GUJARATI" },
-                                        { value: "BENGALI", label: "BENGALI" },
-                                        { value: "TAMIL", label: "TAMIL" },
-                                        { value: "TELUGU", label: "TELUGU" },
-                                        { value: "KANNADA", label: "KANNADA" },
-                                        { value: "MALAYALAM", label: "MALAYALAM" },
-                                        { value: "PUNJABI", label: "PUNJABI" },
-                                        { value: "URDU", label: "URDU" },
-                                        { value: "OTHER", label: "OTHER" },
-                                    ]}
+                                    options={LANGUAGES}
                                 />
 
                                 <div className="flex-1">
@@ -354,10 +341,7 @@ const StudentAdmissionForm = () => {
                                         required
                                         register={register}
                                         errors={errors}
-                                        options={districts.map((district) => ({
-                                            value: district,
-                                            label: district,
-                                        }))}
+                                        options={DISTRICTS}
                                     />
                                 </div>
                             </div>
@@ -465,30 +449,7 @@ const StudentAdmissionForm = () => {
                                     required
                                     register={register}
                                     errors={errors}
-                                    options={[
-                                        { value: "SCHOOL STUDENT", label: "SCHOOL STUDENT" },
-                                        { value: "COLLEGE STUDENT", label: "COLLEGE STUDENT" },
-                                        { value: "TEACHER", label: "TEACHER" },
-                                        { value: "EMPLOYEE", label: "EMPLOYEE" },
-                                        { value: "SELF EMPLOYED", label: "SELF EMPLOYED" },
-                                        { value: "HOUSEWIFE", label: "HOUSEWIFE" },
-                                        { value: "UNEMPLOYED", label: "UNEMPLOYED" },
-                                        { value: "RETIRED", label: "RETIRED" },
-                                        { value: "FARMER", label: "FARMER" },
-                                        { value: "GOVT EMPLOYEE", label: "GOVT EMPLOYEE" },
-                                        { value: "INDUSTRIAL WORKER", label: "INDUSTRIAL WORKER" },
-                                        {
-                                            value: "BUILDING CONSTRUCTION WORKER",
-                                            label: "BUILDING CONSTRUCTION WORKER",
-                                        },
-                                        {
-                                            value: "APPLICANT OF COMPETITIVE EXAMS (MPSC/UPSC)",
-                                            label: "APPLICANT OF COMPETITIVE EXAMS (MPSC/UPSC)",
-                                        },
-                                        { value: "SENIOR CITIZEN", label: "SENIOR CITIZEN" },
-                                        { value: "TRADER", label: "TRADER" },
-                                        { value: "OTHER", label: "OTHER" },
-                                    ]}
+                                    options={CATEGORY}
                                 />
 
                                 <FormSelect
@@ -498,22 +459,7 @@ const StudentAdmissionForm = () => {
                                     required
                                     register={register}
                                     errors={errors}
-                                    options={[
-                                        { value: "1st-4th STD", label: "1st-4th STD." },
-                                        { value: "5th STD", label: "5th STD." },
-                                        { value: "6th STD", label: "6th STD." },
-                                        { value: "7th STD", label: "7th STD." },
-                                        { value: "8th STD", label: "8th STD." },
-                                        { value: "9th STD", label: "9th STD." },
-                                        { value: "10th STD", label: "10th STD." },
-                                        { value: "11th STD", label: "11th STD." },
-                                        { value: "12th STD", label: "12th STD." },
-                                        { value: "DIPLOMA", label: "DIPLOMA" },
-                                        { value: "FY-TY", label: "FY to TY" },
-                                        { value: "GRADUATE", label: "GRADUATE" },
-                                        { value: "POST GRADUATE", label: "POST GRADUATE" },
-                                        { value: "OTHER", label: "OTHER" },
-                                    ]}
+                                    options={EDUCATIONAL_QUALIFICATION}
                                 />
 
                                 <FormSelect
@@ -523,22 +469,7 @@ const StudentAdmissionForm = () => {
                                     required
                                     register={register}
                                     errors={errors}
-                                    options={[
-                                        { value: "MS-CIT", label: "MS-CIT" },
-                                        { value: "ADVANCE TALLY - CIT", label: "ADVANCE TALLY - CIT" },
-                                        { value: "ADVANCE TALLY - KLIC", label: "ADVANCE TALLY - KLIC" },
-                                        { value: "ADVANCE EXCEL - CIT", label: "ADVANCE EXCEL - KLIC" },
-                                        { value: "ENGLISH TYPING - MKCL", label: "ENGLISH TYPING - MKCL" },
-                                        { value: "ENGLISH TYPING - CIT", label: "ENGLISH TYPING - CIT" },
-                                        { value: "ENGLISH TYPING - GOVT", label: "ENGLISH TYPING - GOVT" },
-                                        { value: "MARATHI TYPING - MKCL", label: "MARATHI TYPING - MKCL" },
-                                        { value: "MARATHI TYPING - CIT", label: "MARATHI TYPING - CIT" },
-                                        { value: "MARATHI TYPING - GOVT", label: "MARATHI TYPING - GOVT" },
-                                        { value: "DTP - CIT", label: "DTP - CIT" },
-                                        { value: "DTP - KLIC", label: "DTP - KLIC" },
-                                        { value: "IT - KLIC", label: "IT - KLIC" },
-                                        { value: "KLIC DIPLOMA", label: "KLIC DIPLOMA" },
-                                    ]}
+                                    options={COURSES}
                                 />
 
                                 <FormSelect
@@ -548,22 +479,7 @@ const StudentAdmissionForm = () => {
                                     required
                                     register={register}
                                     errors={errors}
-                                    options={[
-                                        { value: "8AM-9AM", label: "8AM to 9AM" },
-                                        { value: "9AM-10AM", label: "9AM to 10AM" },
-                                        { value: "10AM-11AM", label: "10AM to 11AM" },
-                                        { value: "11AM-12PM", label: "11AM to 12PM" },
-                                        { value: "12PM-1PM", label: "12PM to 1PM" },
-                                        { value: "1PM-2PM", label: "1PM to 2PM" },
-                                        { value: "2PM-3PM", label: "2PM to 3PM" },
-                                        { value: "3PM-4PM", label: "3PM to 4PM" },
-                                        { value: "4PM-5PM", label: "4PM to 5PM" },
-                                        { value: "5PM-6PM", label: "5PM to 6PM" },
-                                        { value: "6PM-7PM", label: "6PM to 7PM" },
-                                        { value: "7PM-8PM", label: "7PM to 8PM" },
-                                        { value: "8PM-9PM", label: "8PM to 9PM" },
-                                        { value: "9PM-10PM", label: "9PM to 10PM" },
-                                    ]}
+                                    options={TIMINGS}
                                 />
 
                                 <FormInput
--- src/components/StudentEnquiryForm.jsx
+++ src/components/StudentEnquiryForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useForm, Controller } from "react-hook-form";
-import { districts } from "./districts";
+import { DISTRICTS, LANGUAGES, CATEGORY, COURSES, EDUCATIONAL_QUALIFICATION, TIMINGS } from "./FormComponents";
 import { FormInput, AadharInput, FormSelect } from "./FormComponents";
 import { toast } from 'react-toastify';
 
@@ -165,20 +165,7 @@ const StudentEnquiryForm = () => {
                                     required
                                     register={register}
                                     errors={errors}
-                                    options={[
-                                        { value: "MARATHI", label: "MARATHI" },
-                                        { value: "HINDI", label: "HINDI" },
-                                        { value: "ENGLISH", label: "ENGLISH" },
-                                        { value: "GUJARATI", label: "GUJARATI" },
-                                        { value: "BENGALI", label: "BENGALI" },
-                                        { value: "TAMIL", label: "TAMIL" },
-                                        { value: "TELUGU", label: "TELUGU" },
-                                        { value: "KANNADA", label: "KANNADA" },
-                                        { value: "MALAYALAM", label: "MALAYALAM" },
-                                        { value: "PUNJABI", label: "PUNJABI" },
-                                        { value: "URDU", label: "URDU" },
-                                        { value: "OTHER", label: "OTHER" },
-                                    ]}
+                                    options={LANGUAGES}
                                 />
 
                                 <div className="flex-1">
@@ -248,10 +235,7 @@ const StudentEnquiryForm = () => {
                                         label="District"
                                         placeholder="Select district"
                                         required
-                                        options={districts.map((district) => ({
-                                            value: district,
-                                            label: district,
-                                        }))}
+                                        options={DISTRICTS}
                                         register={register}
                                         errors={errors}
                                     />
@@ -361,30 +345,7 @@ const StudentEnquiryForm = () => {
                                     required
                                     register={register}
                                     errors={errors}
-                                    options={[
-                                        { value: "SCHOOL STUDENT", label: "SCHOOL STUDENT" },
-                                        { value: "COLLEGE STUDENT", label: "COLLEGE STUDENT" },
-                                        { value: "TEACHER", label: "TEACHER" },
-                                        { value: "EMPLOYEE", label: "EMPLOYEE" },
-                                        { value: "SELF EMPLOYED", label: "SELF EMPLOYED" },
-                                        { value: "HOUSEWIFE", label: "HOUSEWIFE" },
-                                        { value: "UNEMPLOYED", label: "UNEMPLOYED" },
-                                        { value: "RETIRED", label: "RETIRED" },
-                                        { value: "FARMER", label: "FARMER" },
-                                        { value: "GOVT EMPLOYEE", label: "GOVT EMPLOYEE" },
-                                        { value: "INDUSTRIAL WORKER", label: "INDUSTRIAL WORKER" },
-                                        {
-                                            value: "BUILDING CONSTRUCTION WORKER",
-                                            label: "BUILDING CONSTRUCTION WORKER",
-                                        },
-                                        {
-                                            value: "APPLICANT OF COMPETITIVE EXAMS (MPSC/UPSC)",
-                                            label: "APPLICANT OF COMPETITIVE EXAMS (MPSC/UPSC)",
-                                        },
-                                        { value: "SENIOR CITIZEN", label: "SENIOR CITIZEN" },
-                                        { value: "TRADER", label: "TRADER" },
-                                        { value: "OTHER", label: "OTHER" },
-                                    ]}
+                                    options={CATEGORY}
                                 />
 
                                 <FormSelect
@@ -394,22 +355,7 @@ const StudentEnquiryForm = () => {
                                     required
                                     register={register}
                                     errors={errors}
-                                    options={[
-                                        { value: "1st-4th STD", label: "1st-4th STD." },
-                                        { value: "5th STD", label: "5th STD." },
-                                        { value: "6th STD", label: "6th STD." },
-                                        { value: "7th STD", label: "7th STD." },
-                                        { value: "8th STD", label: "8th STD." },
-                                        { value: "9th STD", label: "9th STD." },
-                                        { value: "10th STD", label: "10th STD." },
-                                        { value: "11th STD", label: "11th STD." },
-                                        { value: "12th STD", label: "12th STD." },
-                                        { value: "DIPLOMA", label: "DIPLOMA" },
-                                        { value: "FY-TY", label: "FY to TY" },
-                                        { value: "GRADUATE", label: "GRADUATE" },
-                                        { value: "POST GRADUATE", label: "POST GRADUATE" },
-                                        { value: "OTHER", label: "OTHER" },
-                                    ]}
+                                    options={EDUCATIONAL_QUALIFICATION}
                                 />
 
                                 <FormSelect
@@ -419,22 +365,7 @@ const StudentEnquiryForm = () => {
                                     required
                                     register={register}
                                     errors={errors}
-                                    options={[
-                                        { value: "MS-CIT", label: "MS-CIT" },
-                                        { value: "ADVANCE TALLY - CIT", label: "ADVANCE TALLY - CIT" },
-                                        { value: "ADVANCE TALLY - KLIC", label: "ADVANCE TALLY - KLIC" },
-                                        { value: "ADVANCE EXCEL - CIT", label: "ADVANCE EXCEL - KLIC" },
-                                        { value: "ENGLISH TYPING - MKCL", label: "ENGLISH TYPING - MKCL" },
-                                        { value: "ENGLISH TYPING - CIT", label: "ENGLISH TYPING - CIT" },
-                                        { value: "ENGLISH TYPING - GOVT", label: "ENGLISH TYPING - GOVT" },
-                                        { value: "MARATHI TYPING - MKCL", label: "MARATHI TYPING - MKCL" },
-                                        { value: "MARATHI TYPING - CIT", label: "MARATHI TYPING - CIT" },
-                                        { value: "MARATHI TYPING - GOVT", label: "MARATHI TYPING - GOVT" },
-                                        { value: "DTP - CIT", label: "DTP - CIT" },
-                                        { value: "DTP - KLIC", label: "DTP - KLIC" },
-                                        { value: "IT - KLIC", label: "IT - KLIC" },
-                                        { value: "KLIC DIPLOMA", label: "KLIC DIPLOMA" },
-                                    ]}
+                                    options={COURSES}
                                 />
 
                                 <FormSelect
@@ -444,22 +375,7 @@ const StudentEnquiryForm = () => {
                                     required
                                     register={register}
                                     errors={errors}
-                                    options={[
-                                        { value: "8AM-9AM", label: "8AM to 9AM" },
-                                        { value: "9AM-10AM", label: "9AM to 10AM" },
-                                        { value: "10AM-11AM", label: "10AM to 11AM" },
-                                        { value: "11AM-12PM", label: "11AM to 12PM" },
-                                        { value: "12PM-1PM", label: "12PM to 1PM" },
-                                        { value: "1PM-2PM", label: "1PM to 2PM" },
-                                        { value: "2PM-3PM", label: "2PM to 3PM" },
-                                        { value: "3PM-4PM", label: "3PM to 4PM" },
-                                        { value: "4PM-5PM", label: "4PM to 5PM" },
-                                        { value: "5PM-6PM", label: "5PM to 6PM" },
-                                        { value: "6PM-7PM", label: "6PM to 7PM" },
-                                        { value: "7PM-8PM", label: "7PM to 8PM" },
-                                        { value: "8PM-9PM", label: "8PM to 9PM" },
-                                        { value: "9PM-10PM", label: "9PM to 10PM" },
-                                    ]}
+                                    options={TIMINGS}
                                 />
                             </div>
                         </div>
