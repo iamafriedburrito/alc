13027ba5dbb7085f055c20df81206b130e392484
blacklytning <blacklytning@yandex.com>
2025-07-06 00:30:37 +0530

    Rename session

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router"
 import { ToastContainer } from "react-toastify"
-import StudentEnquiryForm from "./components/StudentEnquiryForm"
-import StudentAdmissionForm from "./components/StudentAdmissionForm"
-import StudentEnquiriesList from "./components/StudentEnquiriesList"
-import StudentAdmissionsList from "./components/StudentAdmissionsList"
+import EnquiryForm from "./components/EnquiryForm"
+import AdmissionForm from "./components/AdmissionForm"
+import EnquiriesList from "./components/EnquiriesList"
+import StudentList from "./components/StudentList"
 import InstituteLogin from "./components/InstituteLogin"
 import CoursesManagement from "./components/CoursesManagement"
 import SettingsPage from "./components/SettingsPage"
@@ -19,10 +19,10 @@ function App() {
             <Routes>
                 <Route path="/" element={<AppLayout />}>
                     <Route index element={<Dashboard />} />
-                    <Route path="enquiry" element={<StudentEnquiryForm />} />
-                    <Route path="admission" element={<StudentAdmissionForm />} />
-                    <Route path="enquiries" element={<StudentEnquiriesList />} />
-                    <Route path="admissions" element={<StudentAdmissionsList />} />
+                    <Route path="enquiry" element={<EnquiryForm />} />
+                    <Route path="admission" element={<AdmissionForm />} />
+                    <Route path="enquiries" element={<EnquiriesList />} />
+                    <Route path="admissions" element={<StudentList />} />
                     <Route path="courses" element={<CoursesManagement />} />
                     <Route path="fees" element={<FeeManagement />} />
                     <Route path="settings" element={<SettingsPage />} />
